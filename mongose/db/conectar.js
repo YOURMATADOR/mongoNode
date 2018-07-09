0ac17fd6f64d77894f2aa2ce97be7af1ec98ffda
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2018-07-09 14:05:11 -0500

    correguir mongodb

--- mongose/db/conectar.js
+++ mongose/db/conectar.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect((process.env.MONGODB_URL) || "mongodb://localhost:27017/mogose");
+mongoose.connect((process.env.MONGODB_URI) || "mongodb://localhost:27017/mogose");
 //conectar a una base de datos desde mongoose
 
 
